d2d09d400c31619ebd85e527743efb550832cd8e
Eoghan <eoghan.odonnell@enview.com>
2021-12-28 15:50:37 -0800

    checkpoint: more impl + tests around guesses

--- hangman/hangman/test/impl_game_test.exs
+++ hangman/hangman/test/impl_game_test.exs
@@ -27,4 +27,40 @@ defmodule HangmanImplGameTest do
     end
   end
 
+  test "a duplicate letter is reported" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    {game, _tally} = Game.make_move(game, "y")
+    assert game.game_state != :already_used
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
+  test "letters used are reported" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game,"x")
+    {game, _tally} = Game.make_move(game,"y")
+    {game, _tally} = Game.make_move(game,"x")
+    assert MapSet.equal?(game.used, MapSet.new(["x", "y"]))
+
+  end
+
+  test "we recognize a letter in the word" do
+    game = Game.new_game("wombat")
+    { game, tally} = Game.make_move(game, "m")
+    assert tally.game_state == :good_guess
+    { game, tally} = Game.make_move(game, "t")
+    assert tally.game_state == :good_guess
+  end
+
+  test "we recognize a letter not in the word" do
+    game = Game.new_game("wombat")
+    { game, tally} = Game.make_move(game, "m")
+    assert tally.game_state == :good_guess
+    { game, tally} = Game.make_move(game, "x")
+    assert tally.game_state == :bad_guess
+  end
+
+
 end
