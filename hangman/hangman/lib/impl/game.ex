54c6608cffc8fef76c32ddac7d1c5e6552da3141
Eoghan <eoghan.odonnell@enview.com>
2021-12-28 16:33:57 -0800

    checkpoint: make the failing test work - revealing letters guessed

--- hangman/hangman/lib/impl/game.ex
+++ hangman/hangman/lib/impl/game.ex
@@ -69,9 +69,15 @@ defmodule Hangman.Impl.Game do
     %{
       turns_left: game.turns_left,
       game_state: game.game_state,
-      letters: [],
+      letters: reveal_guessed_letters(game),
       used: game.used |> MapSet.to_list |> Enum.sort
     }
+  end
+
+  defp reveal_guessed_letters(game) do
+    game.letters
+    |> Enum.map(fn letter -> MapSet.member?(game.used, letter)
+        |> maybe_reveal(letter) end)
   end
 
   defp return_with_tally(game) do
@@ -80,5 +86,8 @@ defmodule Hangman.Impl.Game do
 
   defp maybe_won(true),  do: :won
   defp maybe_won(_),     do: :good_guess
+  defp maybe_reveal(true, letter),  do: letter
+  defp maybe_reveal(_, _letter),     do: "_"
+
 
 end
