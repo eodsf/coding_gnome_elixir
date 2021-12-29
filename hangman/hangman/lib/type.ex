cd5a9af4c5d4d32d7785c053af8a410c10bb7b72
Eoghan <eoghan.odonnell@enview.com>
2021-12-29 15:45:37 -0800

    adding initial logic into text client 'game loop'

--- hangman/hangman/lib/type.ex
+++ hangman/hangman/lib/type.ex
@@ -1,6 +1,6 @@
 defmodule Hangman.Type do
 
-  @type state :: :iinitializing | :won | :lost | :good_guess | :bad_guess | :already_used
+  @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
 
   @type tally :: %{
     turns_left: integer,
--- hangman/text_client/lib/impl/player.ex
+++ hangman/text_client/lib/impl/player.ex
@@ -11,15 +11,33 @@ defmodule TextClient.Impl.Player do
     interact( {game, tally} )
   end
 
-  @spec interact(state) :: :ok
-  def interact do
-    #feedback
-    #diaply current word
-    #get next guess
-    #make move
-    interact()
-
+  @spec interact( {game, tally} ) :: :ok
+  def interact( {_game, _tally = %{ game_state: :won} } ) do
+    IO.puts "Congrats, you won the game"
   end
 
 
+  def interact( {_game, tally = %{ game_state: :lost} } ) do
+    IO.puts "Commiserations, you lost the game - the word was #{tally.letters |> Enum.join}"
+  end
+
+
+ def interact( {game, tally} ) do
+    #feedback
+    IO.puts feedback_for(tally)
+    #diaply current word
+    #get next guess
+    #make move
+    # commented out for now
+    #interact()
+
+  end
+
+  def feedback_for(tally = %{game_state: :initializing }) do
+    IO.puts "Welcome to the game. The word to guess has #{tally.letters |> length}"
+  end
+
+  def feedback_for(%{ game_state: :good_guess}), do: IO.puts "Good guess!"
+  def feedback_for(%{ game_state: :bad_guess}), do: IO.puts "Bad guess! That letter is not in the word"
+  def feedback_for(%{ game_state: :already_used}), do: IO.puts "Sorry, you alrady used that letter."
 end
